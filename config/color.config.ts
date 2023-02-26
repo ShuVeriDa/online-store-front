c369e0d81f4d79a135eb183748f43e0243d9acf6
ShuVeriDa <bashtarov@outlook.com>
2023-02-26 14:37:58 +0300

    added styles

---
+++ config/color.config.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  purple: '#6160DC',
+  'light-blue': '#03DAF2',
+  orange: '#FFB74A',
+  green: '#008d64',
+  'dark-green': '#006044',
+  'light-green': '#e6f2ef',
+  black: '#222222',
+  white: '#ffffff',
+  red: '#f23c3d',
+  gray: '#e8e7e3',
+  beige: "#a49b8f",
+  'light-gray': '#F5F5F5'
+}
--- src/app/components/ui/catalog/carousel/carousel-item/CarouselItem.tsx
+++ src/app/components/ui/catalog/carousel/carousel-item/CarouselItem.tsx
@@ -3,30 +3,61 @@ import {IProduct} from "@/app/types/product.interface";
 import Image from "next/image";
 import styles from '../Carousel.module.scss';
 import cn from 'clsx'
+import * as url from "url";
+import {background, Button} from "@chakra-ui/react";
+import {useActions} from "@/app/hooks/useActions";
+import {COLORS} from "../../../../../../../config/color.config";
 
 
-
-export const CarouselItem: FC<{product: IProduct}> = ({product}) => {
+export const CarouselItem: FC<{ product: IProduct }> = ({product}) => {
   const isActive = product.id === 2
+  const {addToCart} = useActions()
 
   return (
     <div className={cn(styles.item, {
       [styles.active]: isActive
     })}>
-      <Image className={styles.image}
-             src={product.images[0]}
-             alt={product.name}
-             width={315}
-             height={315}
-      />
-      
-      <div className={styles.heading}>
-        <div>{product.name}</div>
-      </div>
-      {!isActive &&  <div className={styles.description}>
-        {product.description}
-      </div>}
+      <div>
+        <Image className={styles.image}
+               src={product.images[0]}
+               alt={product.name}
+               width={315}
+               height={315}
+        />
 
+        <div className={styles.heading}>
+          <div>{product.name}</div>
+        </div>
+        {isActive
+          ? (
+            <>
+              {/*TODO: не отображается в центре*/}
+              <div className='text-center'>
+                <Button
+                  onClick={() => addToCart({
+                    product: product,
+                    quantity: 1
+                  })}
+                  color={COLORS.green}
+                  className='tracking-widest'
+                  marginTop={10}
+                  borderRadius={20}
+                  fontWeight={500}
+                  textTransform={"uppercase"}
+                  fontSize={12}
+                >
+                  Add to Basket
+                </Button>
+              </div>
+            </>
+          )
+
+          : (
+            <div className={styles.description}>
+              {product.description}
+            </div>
+          )}
+      </div>
     </div>
   );
 };
